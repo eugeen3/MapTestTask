f7ada8b7c17fb1342e907b588271d4f4efa59e1a
Eugene <vvgg42@gmail.com>
2022-10-11 15:43:32 +0300

    add marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:map_test_task/presentation/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,45 +13,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: const MaterialColor(0xffFFC137, {
+          50: Color.fromRGBO(255, 193, 55, .1),
+          100: Color.fromRGBO(255, 193, 55, .2),
+          200: Color.fromRGBO(255, 193, 55, .3),
+          300: Color.fromRGBO(255, 193, 55, .4),
+          400: Color.fromRGBO(255, 193, 55, .5),
+          500: Color.fromRGBO(255, 193, 55, .6),
+          600: Color.fromRGBO(255, 193, 55, .7),
+          700: Color.fromRGBO(255, 193, 55, .8),
+          800: Color.fromRGBO(255, 193, 55, .9),
+          900: Color.fromRGBO(255, 193, 55, 1),
+        }),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(title: 'Allsports Map'),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FlutterMap(
-          options: MapOptions(
-            center: LatLng(53.90522, 27.55386),
-            zoom: 13,
-            interactiveFlags: ~InteractiveFlag.rotate,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              userAgentPackageName: 'com.example.map_test_task',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-//53.9052862767787, 27.553548263623615
